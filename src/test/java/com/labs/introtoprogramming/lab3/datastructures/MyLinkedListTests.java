287f8afd6bab2588d735df2a74c103a14b2ae317
Nikita Volobuev <nikitavbv@gmail.com>
2019-03-22 23:42:17 +0200

    refactor(MyLinkedListTests): remove unused import

--- src/test/java/com/labs/introtoprogramming/lab3/datastructures/MyLinkedListTests.java
+++ src/test/java/com/labs/introtoprogramming/lab3/datastructures/MyLinkedListTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import org.junit.Test;
 
