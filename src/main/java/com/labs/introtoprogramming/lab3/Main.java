3d651b3948c8647a099a33eaefa89f9dc0c1da69
Brook333 <maher5ter270301@gmail.com>
2019-03-23 01:36:34 +0200

    feat(main): add user input processing

--- src/main/java/com/labs/introtoprogramming/lab3/Main.java
+++ src/main/java/com/labs/introtoprogramming/lab3/Main.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
   private static MyHashTable<String, String> dictionary;
   private static final String DELIMITER = "----------------------------------------------------------------";
   private static final String INPUT_FILE_NAME = "dict_processed.txt";
   private static final int DICTIONARY_CAPACITY= 100000;
+  private static final String TOKEN_TO_LEAVE = ":q";
 
   public static void main(String[] args) {
     dictionary = new MyHashTable<>(DICTIONARY_CAPACITY);
     File file = new File(INPUT_FILE_NAME);
     loadToDictionary(file, dictionary);
+    processUserInput();
   }
 
   /**
@@ -82,5 +85,37 @@ public class Main {
       if (Character.isLowerCase(c)) return false;
     }
     return true;
+  }
+
+  /**
+   * Process user input to get word definition from dictionary
+   *
+   */
+  private static void processUserInput() {
+    Scanner scanner = new Scanner(System.in);
+    String token;
+    System.out.println("Print :q to leave");
+    System.out.print("\nType a sentence to get definition: ");
+
+    while ((token = scanner.nextLine()) != null) {
+      if (token.equals(TOKEN_TO_LEAVE)) return;
+      String def = findWordInDictionary(token);
+      if (def == null) System.out.println("Sorry " + token + " cannot be found");
+      else {
+        System.out.println(token);
+        System.out.println(def);
+      }
+      System.out.print("\nType a sentence to get definition: ");
+    }
+  }
+
+  /**
+   * Find word definition in dictionary
+   *
+   * @param word word to search
+   * @return definition of word in dictionary or null if it is not there
+   */
+  private static String findWordInDictionary(String word) {
+    return dictionary.getOrDefault(word.toUpperCase(), null);
   }
 }
