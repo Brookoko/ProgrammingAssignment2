44fea0303670435e87d912b94ce52fd4209250d0
Nikita Volobuev <nikitavbv@gmail.com>
2019-03-23 00:21:49 +0200

    feat(MyHashTable): Hash Table implementation

---
+++ src/main/java/com/labs/introtoprogramming/lab3/datastructures/MyHashTableInterface.java
@@ -0,0 +1,58 @@
+package com.labs.introtoprogramming.lab3.datastructures;
+
+import java.util.Optional;
+
+/**
+ * Data structure which stores key-value pairs.
+ * Cannot contain duplicate keys, each key maps to one value.
+ *
+ * @param <K> the type of keys in this hash table.
+ * @param <V> the type of values in this hash table.
+ */
+public interface MyHashTableInterface<K, V> {
+
+  /**
+   * Sets value for the key.
+   *
+   * @param key key to set value for
+   * @param value value to set
+   */
+  public void put(K key, V value);
+
+  /**
+   * Get value by key, or empty optional if no such key found in this hash table.
+   *
+   * @param key key to get value for
+   * @return value for this key
+   */
+  public Optional<V> get(K key);
+
+  /**
+   * Get value by key, or default value of no such key found in this hash table.
+   *
+   * @param key ket to get value for
+   * @param defaultValue value to return if key does not exist in this hash table
+   * @return value for this key
+   */
+  public V getOrDefault(K key, V defaultValue);
+
+  /**
+   * Remove key from hash table.
+   *
+   * @param key key to remove
+   */
+  public void remove(K key);
+
+  /** Returns <tt>true</tt> if this hash table contains value for specific key. */
+  public boolean containsKey(K key);
+
+  /** Remove all values from this hash table. */
+  public void clear();
+
+  /** Returns <tt>true</tt> if this hash table contains no elements. */
+  boolean isEmpty();
+
+  /** Returns the number of values in this hash table. */
+  int getSize();
+
+}
---
+++ src/test/java/com/labs/introtoprogramming/lab3/datastructures/HashTableTests.java
@@ -0,0 +1,113 @@
+package com.labs.introtoprogramming.lab3.datastructures;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HashTableTests {
+
+  @Test
+  public void testPut() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    assertEquals(1, myHashTable.getSize());
+    myHashTable.put("b", 2);
+    assertEquals(2, myHashTable.getSize());
+
+    assertEquals(2, myHashTable.get("b").orElseThrow(AssertionError::new).intValue());
+    assertEquals(1, myHashTable.get("a").orElseThrow(AssertionError::new).intValue());
+    assertEquals(2, myHashTable.getSize());
+  }
+
+  @Test
+  public void testGetNonExistingKey() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    assertFalse(myHashTable.get("foobar").isPresent());
+  }
+
+  @Test
+  public void testGetNonExistingKeyByExistingHash() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>(2);
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+    assertFalse(myHashTable.get("foobar").isPresent());
+  }
+
+  @Test
+  public void testGetOrDefault() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+    assertEquals(42, myHashTable.getOrDefault("foobar", 42).intValue());
+  }
+
+  @Test
+  public void testRemoveKey() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+    myHashTable.remove("b");
+    assertEquals(2, myHashTable.getSize());
+    assertFalse(myHashTable.containsKey("b"));
+  }
+
+  @Test
+  public void testContainsKey() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    assertTrue(myHashTable.containsKey("a"));
+    assertFalse(myHashTable.containsKey("b"));
+  }
+  
+  @Test
+  public void testRemovingNonexistentKey() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+    myHashTable.remove("d");
+    assertEquals(3, myHashTable.getSize());
+  }
+
+  @Test
+  public void testIsEmpty() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertTrue(myHashTable.isEmpty());
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    assertFalse(myHashTable.isEmpty());
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+  }
+
+  @Test
+  public void testClear() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    assertEquals(0, myHashTable.getSize());
+    myHashTable.put("a", 1);
+    myHashTable.put("b", 2);
+    myHashTable.put("c", 3);
+    assertEquals(3, myHashTable.getSize());
+    myHashTable.clear();
+    assertEquals(0, myHashTable.getSize());
+    assertFalse(myHashTable.containsKey("a"));
+  }
+}
