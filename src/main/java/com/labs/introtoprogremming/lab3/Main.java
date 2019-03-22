bbd98e11a338f09bf7eaa0274b1a2692a80824da
Brook333 <maher5ter270301@gmail.com>
2019-03-23 00:59:37 +0200

    feat(main): add load of words definition to dictionary from file

--- src/main/java/com/labs/introtoprogremming/lab3/Main.java
+++ src/main/java/com/labs/introtoprogremming/lab3/Main.java
@@ -1,4 +1,82 @@
 package com.labs.introtoprogremming.lab3;
 
+import com.labs.introtoprogramming.lab3.datastructures.MyHashTable;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
+  private static MyHashTable<String, String> dictionary;
+  private static final String DELIMITER = "----------------------------------------------------------------";
+  private static final String INPUT_FILE_NAME = "dict_processed.txt";
+
+  public static void main(String[] args) {
+    dictionary = new MyHashTable<>(10000000);
+    File file = new File(INPUT_FILE_NAME);
+    loadToDictionary(file, dictionary);
+  }
+
+  /**
+   * Load to dictionary from file
+   *
+   * @param file file with words and their definition (txt)
+   * @param dict hast table to which load words
+   */
+  private static void loadToDictionary(File file, MyHashTable<String, String> dict) {
+    try {
+      loadToDictionary(new FileInputStream(file), dict);
+    } catch (IOException e) {
+      System.out.println("Failed to read file");
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  /**
+   * Load to dictionary from txt data input stream
+   *
+   * @param in input stream with words definition data
+   * @param dict hash table to which load words
+   * @throws IOException if stream read failed
+   */
+  private static void loadToDictionary(InputStream in, MyHashTable<String, String> dict) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+    String line;
+    String token = "";
+    StringBuilder content = new StringBuilder();
+    while ((line = bf.readLine()) != null) {
+      if (isLineUpperCase(line)) {
+        if (!token.isEmpty() && content.length() != 0) {
+          if (dict.containsKey(token)) {
+            content.insert(0, DELIMITER)
+                    .insert(0, "\n")
+                    .insert(0, dict.get(token));
+          }
+          dict.put(token, content.toString());
+        }
+        token = line;
+        content = new StringBuilder();
+      } else if (!line.isEmpty()) content.append("\n").append(line);
+    }
+    bf.close();
+  }
+
+  /**
+   * Check is string consists only of uppercase characters
+   *
+   * @param line string to be checked
+   * @return true if string consist only of uppercase characters
+   */
+  private static boolean isLineUpperCase(String line) {
+    if (line.isEmpty()) return false;
+    for (int i = 0; i < line.length(); i++) {
+      if (Character.isLowerCase(line.charAt(i))) return false;
+    }
+    return true;
+  }
 }
